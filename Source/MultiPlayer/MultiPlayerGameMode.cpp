a8fab3ca0443b7ad544bd077b335534cfdf932bf
AFeiYA <386665678@qq.com>
2019-06-22 16:52:48 +0800

    Initial commit

---
+++ Source/MultiPlayer/MultiPlayerGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "MultiPlayerGameMode.h"
+#include "MultiPlayerCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AMultiPlayerGameMode::AMultiPlayerGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
